865cfef906a76408d52a28cf82790413186bbb90
Young-power <presidentkamso@gmail.com>
2025-09-25 23:19:22 +0000

    ajout de la section projet3 react native

---
+++ src/app/commponents/project/Project3.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { IoLogoReact } from "react-icons/io5";
+import { FaGithub } from "react-icons/fa";
+import Link from 'next/link';
+import Title from '../Title';
+import Image from 'next/image';
+const Project3 = () => {
+    return (
+        <section className='w-full mt-10'>
+            <Title title="Projet 3" style="text-white text-center text-2xl mb-5" />
+            <p className='text-white text-center mb-3 text-xl lg:text-2xl'>WhatsApp Clone (React Native)</p>
+           
+            <div className='w-full flex flex-col lg:flex-row justify-center items-center'>
+                <div className='w-full lg:w-1/2 '>
+                    <Image src={"/assets/projet3.png"} alt="project1" width={600} height={400} className='w-full h-auto p-5 rounded-3xl block' />
+                </div>
+
+
+                <div className='w-full lg:w-1/2 flex justify-center items-center  '>
+                    <div className='w-32 '>
+                        <div className='flex flex-col justify-center items-center '>
+                            <IoLogoReact className='text-[170px] lg:text-[200px]  text-blue-600' />
+                            <p className='text-white text-xl lg:text-2xl text-nowrap font-medium italic'>React Native</p>
+                        </div>
+                        <div className='flex flex-col justify-center items-center mt-7 gap-y-1'>
+                            <FaGithub className='text-white text-5xl ml-2 cursor-pointer animate-bounce duration-1000' />
+                            <button className="btn btn-primary border px-3 lg:px-5 py-0.5 text-white text-nowrap  rounded-4xl ">
+                                <Link href={"https://github.com/Young-power/whatsapp_ReactNative.git"} className='rounded-4xl' > Code disponible ici</Link>
+                            </button>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+        </section>
+    )
+}
+
+export default Project3
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "./commponents/hero/Hero";
 import MyMapWrapper from "./commponents/map/MyMapWrapper";
 import Project from "./commponents/project/Project";
 import Project2 from "./commponents/project/Project2";
+import Project3 from "./commponents/project/Project3";
 import SchoolCarieer from "./commponents/school/SchoolCarieer";
 import SkillSender from "./commponents/skills/SkillSender";
 import Title from "./commponents/Title";
@@ -19,6 +20,7 @@ export default function Home() {
       <Title title="PROJETS" style="text-white text-center text-3xl lg:text-5xl mt-28 border-b-2 border-white pb-3" />
       <Project />
       <Project2 />
+      <Project3 />
       <Title title="Google Map" style="text-white text-center text-3xl lg:text-5xl mt-10 pb-3" />
       <div className="my-10">
         <MyMapWrapper />
