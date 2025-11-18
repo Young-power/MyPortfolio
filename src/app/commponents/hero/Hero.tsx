7541c89b195960fc7ed183ea39323c5390f69603
Mahine_K <presidentkamso@gmail.com>
2025-11-18 00:56:04 +0000

    add yellow rounded border to profile image

--- src/app/commponents/hero/Hero.tsx
+++ src/app/commponents/hero/Hero.tsx
@@ -39,7 +39,10 @@ const Hero = () => {
             <div className="flex w-full flex-col lg:flex-row justify-between  rounded-3xl sm:px-16 lg:px-20 pt-10 border-b-2 border-white">
                 <div className="flex flex-col lg:flex-row justify-center items-center gap-x-5 pb-10">
                     <div className="flex flex-row lg:flex-col justify-center items-center space-x-3 lg:space-y-3 ">
-                        <Image src="/assets/Mahine.jpg" alt="legend" width={200} height={100} className=" object-cover lg:w-96 rounded-full " />
+                        <div className=" rounded-full  p-1 border-4 border-yellow-400">
+                            <Image src="/assets/Mahine.jpg" alt="legend" width={200} height={100} className=" object-cover lg:w-96 rounded-full  " />
+
+                        </div>
                         <SocialMedia />
                     </div>
                     <div className=" flex flex-col items-center justify-center lg:mr-5">
@@ -51,7 +54,7 @@ const Hero = () => {
                 <div className="w-full flex flex-col  justify-center items-center lg:max-w-1/2 lg:border-l-5  lg:border-white text-xl">
                     <h1 className="text-white text-lg pb-3 font-bold italic text-nowrap pl-3 lg:text-xl  ">« Développeur Web & Mobile <span className=" hidden md:inline-block">Passionné</span>  »</h1>
                     <p className="w-full text-sm md:text-md text-wrap text-center lg:text-wrap text-white lg:px-10 lg:text-2xl lg:text-justify ">
-                         Je suis Mahaman Mainnou Kamagaté, développeur full-stack passionné par la création d’applications web et mobiles modernes.
+                        Je suis Mahaman Mainnou Kamagaté, développeur full-stack passionné par la création d’applications web et mobiles modernes.
                         J&apos;aime transformer des idées en solutions concrètes, en alliant performance, design et innovation.
                         Mon objectif : concevoir des expériences numériques utiles, fluides et accessibles à tous. »
                     </p>
