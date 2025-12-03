2b05e9e2d0ec0ae8e0bfa0e139fc8511b60096d6
Mahine_K <presidentkamso@gmail.com>
2025-12-03 11:22:33 +0000

    edit cv page

--- src/app/commponents/cv/Cv.tsx
+++ src/app/commponents/cv/Cv.tsx
@@ -4,75 +4,67 @@ import { FaFilePdf } from 'react-icons/fa';
 import { IoMdDownload } from "react-icons/io";
 import { toast } from 'react-toastify'
 import TippyShow from '../tippy/TippyShow';
+import { useEffect } from 'react';
+
+
 const Cv = () => {
-    const handleDownload = async () => {
 
-        try {
-            const res = await fetch("/assets/files/myCv.pdf");
+ 
+  const handleDownload = async () => {
+    try {
+      const res = await fetch("/assets/files/myCv.pdf");
+      if (!res.ok) throw new Error('Impossible de récupérer le fichier');
 
-            if (!res.ok) throw new Error('Impossible de récupérer le fichier');
+      const blob = await res.blob();
 
-            const blob = await res.blob();
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
 
+      a.href = url;
+      a.download = "mahine_cv.pdf";
+      a.click();
 
-            const url = window.URL.createObjectURL(blob);
-            const a = document.createElement('a');
-
-            a.href = url
-            a.download = "mahine_cv.pdf";
-            a.click();
-
-            window.URL.revokeObjectURL(url);
-            toast.success('Téléchargement réuissi !');
-
-        } catch (error) {
-
-            toast.error('Erreur : le téléchargement a échoué !');
-
-        }
-
+      window.URL.revokeObjectURL(url);
+      toast.success('Téléchargement réuissi !');
+    } catch (error) {
+      toast.error('Erreur : le téléchargement a échoué !');
     }
-    return (
-        <div className='w-full flex flex-col justify-center items-center mt-16  '>
-            <div className='w-96 h-[500px] md:w-fit md:flex md:flex-row p-10 rounded-4xl flex flex-col justify-center items-center gap-7 bg-gradient-to-br from-slate-800 to-slate-950  border border-white'>
-                <div className="w-52 h-52 md:w-72 md:h-72 rounded-full  overflow-hidden border-2 shadow-white-500  border-white-500">
-                    <Image
-                        src="/assets/m.jpeg"
-                        alt="cv image"
-                        width={500}
-                        height={500}
-                        className="object-cover w-96 h-96 pb-32 md:pb-0 md:pt-[2px] bg-white"
-                    />
-                </div>
-                <div className='md:flex md:flex-col md:justify-center md:items-center md:gap-3'>
-                    <TippyShow message="Download" placement='top' animation='scale' >
-                        <button
-                            onClick={handleDownload}
-                            className="bg-gradient-to-tl from-blue-600 to-blue-800 text-white text-md lg:text-xl px-5 py-3 rounded-4xl cursor-pointer inline-flex items-center gap-2 transition-transform hover:translate-y-1 duration-300 ease-in-out"
-                        >
-                            <FaFilePdf size={25} className="text-red-400" />
-                            Télécharger mon CV
-                            <IoMdDownload size={30} className="text-white rounded-full" />
-
-                        </button>
-                    </TippyShow>
+  };
 
 
-                    <p className="text-white text-sm  italic text-center md:text-lg">
-                        Un aperçu complet de mon parcours et de mes compétences.
-                    </p>
 
-
-                </div>
-
-
-            </div>
+  return (
+    <div className='w-full flex flex-col justify-center items-center mt-16'>
+      <div className='w-96 h-[500px] md:w-fit md:flex md:flex-row p-10 rounded-4xl flex flex-col justify-center items-center gap-7 bg-gradient-to-br from-slate-800 to-slate-950 border border-white'>
+        <div className="w-52 h-52 md:w-72 md:h-72 rounded-full overflow-hidden border-2 shadow-white-500 border-white-500">
+          <Image
+            src="/assets/m.jpeg"
+            alt="cv image"
+            width={500}
+            height={500}
+            className="object-cover w-96 h-96 pb-32 md:pb-0 md:pt-[2px] bg-white"
+          />
         </div>
 
+        <div className='flex flex-col justify-center items-center md:gap-3'>
+          <TippyShow message="Download" placement='top' animation='scale'>
+            <button
+              onClick={handleDownload}
+              className="bg-gradient-to-tl from-blue-600 to-blue-800 text-white text-md lg:text-xl px-5 py-3 rounded-4xl cursor-pointer inline-flex items-center gap-2 transition-transform hover:translate-y-1 duration-300 ease-in-out"
+            >
+              <FaFilePdf size={25} className="text-red-400" />
+              Télécharger mon CV
+              <IoMdDownload size={30} className="text-white rounded-full" />
+            </button>
+          </TippyShow>
 
+          <p className="text-white text-sm italic text-center md:text-lg">
+            Un aperçu complet de mon parcours et de mes compétences.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-
-    )
-}
-
-export default Cv
+export default Cv;
