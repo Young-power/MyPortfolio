d0dfc28553627178f229ac418fb28e9c9cebf442
Mahine_K <presidentkamso@gmail.com>
2026-03-01 11:13:36 +0000

    logo email own

--- emails/SendEmailTome.tsx
+++ emails/SendEmailTome.tsx
@@ -18,19 +18,19 @@ type Props = {
 
 export const SendEmailToMe = ({ name, email, phone, project, description }: Props) => {
     const AvatarImg = "https://www.mahine.site/assets/Mahine.jpg";
-    const M2kLogo = "https://www.mahine.site/assets/m2kgroup/m2kGroup.png";
+    const M2kLogo = "https://www.mahine.site/assets/m2kgroup/mkgroup.png";
 
     return (
         <Html>
             <Body className="bg-gray-100 font-sans p-6">
                 <Container className="bg-white rounded-xl shadow-lg max-w-[600px] mx-auto p-6">
-                    
+
                     {/* Logo */}
                     <Section className="text-center mb-6">
                         <Img
                             src={M2kLogo}
-                            width="120"
-                            height="40"
+                            width="150"
+                            height="150"
                             alt="M²K-Group"
                             className="mx-auto"
                         />
